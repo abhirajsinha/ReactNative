d63b59b0b810c6d042f120f2ca60d1bc03644dad
Abhiraj <abhirajsinha25@gmail.com>
2023-06-17 19:42:33 +0530

    removed unwanted permissions from camera.jsx

--- imageapp/Camera.jsx
+++ imageapp/Camera.jsx
@@ -27,115 +27,79 @@ const Camera = () => {
       } catch (err) {
         console.warn(err);
       }
-      try {
-        const granted = await PermissionsAndroid.request(
-          PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
-          {
-            title: 'Gallery Permission',
-            message: 'App needs access to your gallery ',
-            buttonNeutral: 'Ask Me Later',
-            buttonNegative: 'Cancel',
-            buttonPositive: 'OK',
-          },
-        );
-        if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-          console.log('Gallery permission given');
-        } else {
-          console.log('Gallery permission denied');
-        }
-      } catch (error) {}
-      try {
-        const granted = await PermissionsAndroid.request(
-          PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
-          {
-            title: 'Cool Photo App Camera Permission',
-            message:
-              'Cool Photo App needs access to your camera ' +
-              'so you can take awesome pictures.',
-            buttonNeutral: 'Ask Me Later',
-            buttonNegative: 'Cancel',
-            buttonPositive: 'OK',
-          },
-        );
-        if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-          console.log('You can use the camera');
-
-          const data = await this.camera.takePictureAsync();
-          let saveResult = CameraRoll.saveToCameraRoll(data.uri);
-          console.warn('takePicture ', saveResult);
-          console.warn('picture url ', data.uri);
-        } else {
-          console.log('Camera permission denied');
-        }
-      } catch (err) {
-        console.warn(err);
-      }
     };
     requestCameraPermission();
   }, []);
 
-  const pickPicture = () => {
+  const openGallery = () => {
     ImagePicker.openPicker({
       width: 300,
       height: 400,
       cropping: true,
-    }).then(image => {
-      console.log(image.path);
-      setImageUri(image.path);
-    });
+    })
+      .then(image => {
+        console.log(image.path);
+        setImageUri(image.path);
+      })
+      .catch(error => {
+        console.log('Error', error);
+      });
   };
 
   const clickPicture = () => {
     ImagePicker.openCamera({
       width: 500,
       height: 600,
-      mediaType: 'any',
-      useFrontCamera: true,
       cropping: true,
       showCropGuidelines: true,
       // freeStyleCropEnabled:true,
       enableRotationGesture: true,
-    }).then(image => {
-      console.log(image.path);
-      setImageUri(image.path);
-    });
+    })
+      .then(image => {
+        setImageUri(image.path);
+      })
+      .catch(error => {
+        console.log('Error', error);
+      });
   };
 
   return (
     <SafeAreaView>
       {imageUri && (
-        <View>
+        <View style={{flex:1, justifyContent:'center', alignItems:'center', marginTop:350}}>
           <Image style={{height: 300, width: 300}} source={{uri: imageUri}} />
         </View>
       )}
-      <TouchableOpacity
-        onPress={pickPicture}
-        style={{
-          marginTop: 300,
-          height: 50,
-          width: '60%',
-          backgroundColor: 'skyblue',
-          borderRadius: 20,
-          justifyContent: 'center',
-          alignItems: 'center',
-          alignSelf: 'center',
-        }}>
-        <Text style={{fontSize: 20}}>Pick a Image</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        onPress={clickPicture}
-        style={{
-          marginTop: 300,
-          height: 50,
-          width: '60%',
-          backgroundColor: 'skyblue',
-          borderRadius: 20,
-          justifyContent: 'center',
-          alignItems: 'center',
-          alignSelf: 'center',
-        }}>
-        <Text style={{fontSize: 20}}>Pick a Image</Text>
-      </TouchableOpacity>
+      <View style={{}}>
+        <TouchableOpacity
+          onPress={openGallery}
+          style={{
+            marginTop: 300,
+            height: 50,
+            width: '60%',
+            backgroundColor: 'skyblue',
+            borderRadius: 20,
+            justifyContent: 'center',
+            alignItems: 'center',
+            alignSelf: 'center',
+          }}>
+          <Text style={{fontSize: 20}}>Pick a Image</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={clickPicture}
+          style={{
+            marginTop: 20,
+            height: 50,
+            width: '60%',
+            backgroundColor: 'skyblue',
+            borderRadius: 20,
+            justifyContent: 'center',
+            alignItems: 'center',
+            alignSelf: 'center',
+          }}>
+          <Text style={{fontSize: 20}}>Open Camera</Text>
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 };
