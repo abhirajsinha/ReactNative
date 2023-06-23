d987aa39c240260a43dad18ce8cae42c0b4ad7be
Abhiraj <abhirajsinha25@gmail.com>
2023-06-23 23:12:12 +0530

    Implemented Pagination in react-native Yeep! ❤️🚀

--- visioncamera/App.tsx
+++ visioncamera/App.tsx
@@ -1,12 +1,14 @@
 import {View, Text, SafeAreaView} from 'react-native';
 import React from 'react';
 import CameraComp from './Components/CameraComp';
+import Pagination from './Components/Pagination';
 
 const App = () => {
   return (
-    <View style={{flex: 1, backgroundColor: 'black'}}>
-      <CameraComp />
-    </View>
+    // <View style={{flex: 1, backgroundColor: 'black'}}>
+    //   <CameraComp />
+    // </View>
+    <Pagination />
   );
 };
 
